0f1a8f4e247b3bb88a4de822a33735683725d985
Junseok Yoo <junseoky21@gmail.com>
2018-07-22 02:44:49 -0700

    map codes

--- life/views.py
+++ life/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.core import serializers
+from life.models import *
 
 def index(request):
-    return render(request, 'life/index.html')
+    all_groups = Group.objects.all()
+    return render(request, 'life/index.html', {"groups": all_groups})
