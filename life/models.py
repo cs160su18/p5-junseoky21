9da152f83e7857074b43ddb96bac0a4f524583ce
Junseok Yoo <junseoky21@gmail.com>
2018-07-22 03:09:08 -0700

    models.py updated

--- life/models.py
+++ life/models.py
@@ -3,3 +3,30 @@ from django.db import models
 class Group(models.Model):
 	established = models.DateTimeField(auto_now_add=True)
 	name = models.CharField(max_length=50)
+
+
+class Customer(models.Model):
+	name = models.CharField(max_length=64)
+	joined = models.DateTimeField(auto_now_add=True)
+	preference = models.CharField(max_length=512)
+
+class Restaurant(models.Model):
+	name = models.CharField(max_length=64)
+	latitude = models.FloatField()
+	longitude = models.FloatField()
+	rating = models.FloatField()
+	joined = models.DateTimeField(auto_now_add=True)
+
+class Review(models.Model):
+	title = models.CharField(max_length=64)
+	content = models.CharField(max_length=4096)
+	rating = models.FloatField()
+	datetime = models.DateTimeField(auto_now_add=True)
+	verified = models.BooleanField()
+	customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+	restaurant = models.ForeignKey('Restaurant', on_delete=models.CASCADE)
+
+class LocationLogs(models.Model):
+	datetime = models.DateTimeField(auto_now_add=True)
+	latitude = models.FloatField()
+	longitude = models.FloatField()
